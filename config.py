54704740666bd2ab4579085a04157a7b355562aa
shrutssss <shrutiv.jahagirdar@gmail.com>
2026-03-14 02:08:24 +0530

    add config file

---
+++ config.py
@@ -0,0 +1,25 @@
+#Creating dictionary with file extensions as key-value pairs
+
+file_types = {
+    "Text Files": [".txt", ".rtf", ".md"],
+    
+    "Document Files": [".doc", ".docx", ".pdf", ".odt"],
+    
+    "Spreadsheet Files": [".xls", ".xlsx", ".csv", ".ods"],
+    
+    "Presentation Files": [".ppt", ".pptx", ".odp"],
+    
+    "Image Files": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg"],
+    
+    "Audio Files": [".mp3", ".wav", ".aac", ".flac", ".ogg"],
+    
+    "Video Files": [".mp4", ".avi", ".mov", ".mkv", ".wmv"],
+    
+    "Programming Files": [".py", ".c", ".cpp", ".java", ".js", ".html", ".css"],
+    
+    "Archive Files": [".zip", ".rar", ".7z", ".tar", ".gz"],
+    
+    "Database Files": [".sql", ".db", ".sqlite", ".mdb"],
+
+    "Executable Files": [".exe", ".bat", ".sh"]
+}
